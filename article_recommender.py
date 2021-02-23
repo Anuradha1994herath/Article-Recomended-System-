aca3bb79b160a551f6abde22244236d25f69c848
Anuradha Herath <47684298+Anuradha1994herath@users.noreply.github.com>
2021-02-23 11:16:53 +0530

    Update article_recommender.py

--- article_recommender.py
+++ article_recommender.py
@@ -9,23 +9,26 @@ def get_title_from_index(index):
 def get_First_author_from_index(index):
 	return df[df.index == index]["First author"].values[0]
 
+def get_Year_from_index(index):
+	return df[df.index == index]["Year"].values[0]
+
 def get_index_from_title(Title):
 	return df[df.Title == Title]["index"].values[0]
 ##################################################
 
 ##Read CSV File
-df = pd.read_csv("article.csv")
+df = pd.read_csv("article.csv",dtype={'Year':'string'})
 ## print(df.iloc[14, 2])
 ##Select Features
 
-features = ['Title','Abstract','Keywords']
+features = ['Title','Abstract','Keywords','Year']
 ## Create a column in DF which combines all selected features
 for feature in features:
 	df[feature] = df[feature].fillna('')
 
 def combine_features(row):
 	try:
-		return row['Title'] +" "+row['Abstract']+" "+row["Keywords"]
+		return row['Title'] +" "+row['Abstract']+" "+row["Keywords"]+" "+row["Year"]
 	except:
 		print ("Error:", row)	
 
@@ -35,11 +38,16 @@ df["combined_features"] = df.apply(combine_features,axis=1)
 cv = CountVectorizer()
 
 count_matrix = cv.fit_transform(df["combined_features"])
-
+# print(count_matrix)
 ## Compute the Cosine Similarity based on the count_matrix
 cosine_sim = cosine_similarity(count_matrix) 
-## val = input("Enter Title: ") 
+# val = input("Enter Title: ") 
 val = df.iloc[-1]["Title"]
+val2 = df.iloc[-1]["Test1"]
+val3 = df.iloc[-1]["Test2"]
+val4 = df.iloc[-1]["Test3"]
+val5 = df.iloc[-1]["Test4"]
+val6 = df.iloc[-1]["Test5"]
 
 ## Get index of this article from its title
 article_index = get_index_from_title(val)
@@ -48,12 +56,19 @@ similar_articles =  list(enumerate(cosine_sim[article_index]))
 
 ## Get a list of similar articles in descending order of similarity score
 sorted_similar_articles = sorted(similar_articles,key=lambda x:x[1],reverse=True)
+# print(sorted_similar_articles)
+# Sorted_by_year = sorted(sorted_similar_articles)
 # print (similar_articles)
-# Print titles of first 3 articles
-print ("Recomended artcles are:")
+# Print output
+# print ("Recomended artcles are:")
 i=0
+
 for element in sorted_similar_articles:
-		print (get_title_from_index(element[0]) + " By " + get_First_author_from_index(element[0] ))
-		i=i+1
+	if((get_title_from_index(element[0]) != val2) & (get_title_from_index(element[0]) != val3) & (get_title_from_index(element[0]) != val4)):
+		x = get_title_from_index(element[0]) + " By " + get_First_author_from_index(element[0]) + " On " + get_Year_from_index(element[0])
+		print(x)
+		
+		i=i+1	
 		if i>5:
 			break
+		
